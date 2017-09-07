c670e48d20b763789ac25e2322edcacf223feb5d
Johnny <johnathoncymoore@gmail.com>
2017-09-07 10:57:49 -0700

    Finished project

--- quizscript.js
+++ quizscript.js
@@ -1,5 +1,27 @@
-$(document).ready(function(){
-		$("button1").hide(function(){
-			$("#answer1").show();
-		});
+$(document).ready(function() {
+	$("#button1").click(function() {
+		$("#disappear1").toggle();
+	})
+	$("#button2").click(function() {
+		$("#disappear2").toggle();
+	})
+	$("#button3").click(function() {
+		$("#disappear3").toggle();
+	})
+	$("#button4").click(function() {
+		$("#disappear4").toggle();
+	})
+	$("#button5").click(function() {
+		$("#disappear5").toggle();
+	})
+	$("#button6").click(function() {
+		$("#disappear6").toggle();
+		$(".endcolors").toggle();
+	})		
 });
+
+
+
+
+
+
